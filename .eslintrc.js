b9c79509d7429d8a7b9d070071f3ecc704c03897
qtipee <quent.vallat@gmail.com>
2021-04-23 17:52:11 +0200

    Configure ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    extends: [
+        '@nuxtjs',
+    ],
+    plugins: [
+    ],
+    // add your custom rules here
+    rules: {
+        'comma-dangle': ['error', 'only-multiline'],
+        indent: ['warn', 4],
+        'no-trailing-spaces': ['error', { skipBlankLines: true }],
+        semi: ['error', 'always'],
+        'space-before-function-paren': ['warn', 'never'],
+        'vue/html-indent': ['warn', 4],
+    },
+};
