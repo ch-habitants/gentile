aaff809c351d1c6bc351524acffe5f9a381f818e
qtipee <quent.vallat@gmail.com>
2021-04-23 18:46:47 +0200

    Reverse commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,6 @@
 export default {
     // Target: https://go.nuxtjs.dev/config-target
     target: 'static',
-
-    router: {
-        base: '/ch-habitants.github.io/',
-    },
     
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
