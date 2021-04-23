31cb1837e840a0e7e584a34329a5f4349479abca
qtipee <qtipee@users.noreply.github.com>
2021-04-23 16:54:57 +0000

    deploy: 9f8d26154117cc9bac89a7ea601b00c2cca4aaf5

--- _nuxt/static/1619196542/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fch-habitants.github.io\u002F_nuxt\u002Fstatic\u002F1619196542",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fch-habitants.github.io\u002F",assetsPath:"\u002Fch-habitants.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
