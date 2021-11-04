2944e9d5598e63c0f84bfe21d888d8793e0655e7
g-wizzy <pierre.burki@protonmail.com>
2021-11-04 17:34:52 +0100

    Added data and search

---
+++ components/GentileList.vue
@@ -0,0 +1,39 @@
+<template>
+    <table>
+        <tr
+            v-for="(gentile,index) in filteredGentile"
+            :key="index"
+        >
+            <td>
+                {{ gentile.canton }}
+            </td>
+            <td>
+                {{ gentile.commune }}
+            </td>
+            <td>
+                {{ gentile.gentile }}
+            </td>
+        </tr>
+    </table>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    computed: {
+        ...mapGetters([
+            'searchValue',
+            'gentiles'
+        ]),
+
+        filteredGentile() {
+            return this.gentiles.filter((item) => {
+                return (
+                    item.canton.includes(this.searchValue) ||
+                    item.commune.includes(this.searchValue) ||
+                    item.gentile.includes(this.searchValue)
+                );
+            });
+        }
+    }
+};
+</script>
---
+++ components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <input id="searchField" v-model="value" type="text" name="searchValue">
+        <button id="SearchButton">
+            Search
+        </button>
+    </div>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    computed: {
+    ...mapGetters(['searchValue']),
+    value: {
+      set(value) {
+        this.$store.commit('setSearchValue', value);
+      },
+      get() {
+        // Or remove mapState and use this.$store.state.values.example
+        return this.searchValue;
+      }
+    }
+  }
+};
+</script>
