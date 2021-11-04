dea1d73266e521cc9a6f3750165ab53f2aa60f5f
g-wizzy <g-wizzy@users.noreply.github.com>
2021-11-04 19:55:37 +0000

    deploy: cedb770cb102ce63d1703edc8b9f06c2405f24d1

--- _nuxt/static/1619196896/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fch-habitants\u002Fgentile\u002F_nuxt\u002Fstatic\u002F1619196896",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fch-habitants\u002Fgentile\u002F",assetsPath:"\u002Fch-habitants\u002Fgentile\u002F_nuxt\u002F",cdnURL:a}}}}(null));
