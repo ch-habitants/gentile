c65bae44e5805535f9292995c4f211c49cce10d4
Quentin Vallat <quent.vallat@gmail.com>
2021-12-13 19:45:52 +0100

    Improve GentileList design

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -3,7 +3,7 @@
         <input
             v-model="value"
             type="text"
-            class="appearance-none m-auto px-4 py-2 block border border-primary rounded shadow-sm text-center focus:outline-none"
+            class="appearance-none m-auto px-4 py-2 block border border-primary rounded shadow-sm text-center focus:outline-none duration-300"
             placeholder="Rechercher"
         >
     </div>
