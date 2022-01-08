c904bba55c67d3e3fb83f81a11c30081d1b2878a
Quentin Vallat <quent.vallat@gmail.com>
2022-01-08 20:04:28 +0100

    Create CantonSelect component

---
+++ components/CantonSelect.vue
@@ -0,0 +1,154 @@
+<template>
+    <div class="mt-1 relative">
+        <!-- Select -->
+        <button
+            type="button"
+            class="relative w-80 bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            aria-haspopup="canton-select"
+            aria-expanded="true"
+            @click="open = true"
+        >
+            <!-- Selected canton -->
+            <span
+                v-if="selectedCanton"
+                class="flex items-center"
+            >
+                <!-- Flag -->
+                <span class="ml-3 block truncate">
+                    {{ selectedCanton.name }}
+                </span>
+            </span>
+
+            <!-- No canton selected -->
+            <span v-else>
+                <span class="ml-3 block truncate">
+                    Aucun
+                </span>
+            </span>
+
+            <!-- Dropdown icon -->
+            <span class="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                <!-- Heroicon name: solid/selector -->
+                <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+            </span>
+        </button>
+
+        <!-- Options -->
+        <transition
+            enter-active-class="transition ease-out duration-200"
+            enter-class="transform opacity-0 scale-95"
+            enter-to-class="transform opacity-100 scale-100"
+            leave-active-class="transition ease-in duration-100"
+            leave-class="transform opacity-100 scale-100"
+            leave-to-class="transform opacity-0 scale-95"
+        >
+            <ul
+                v-if="open"
+                class="absolute z-10 mt-1 w-80 bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+                tabindex="-1"
+                role="canton-select"
+                aria-activedescendant="canton-select-option-3"
+            >
+                <!-- No selection option -->
+                <li
+                    id="canton-select-option-none"
+                    class="text-primary-900 text-opacity-60 cursor-default select-none relative py-2 pl-6 pr-9 hover:bg-gray-100 duration-300"
+                    role="option"
+                    @click="select('')"
+                >
+                    Aucun
+                </li>
+
+                <!-- List of cantons -->
+                <li
+                    v-for="(canton, index) in cantons"
+                    :id="`canton-select-option-${index}`"
+                    :key="index"
+                    class="text-primary-900 text-opacity-80 cursor-default select-none relative py-2 pl-3 pr-9 hover:bg-gray-100 duration-300"
+                    role="option"
+                    @click="select(canton)"
+                >
+                    <div class="flex items-center">
+                        <!-- Flag -->
+                        <span
+                            class="ml-3 block truncate"
+                            :class="{
+                                'font-semibold': canton.slug === selectedCanton.slug,
+                                'font-normal': canton.slug !== selectedCanton.slug,
+                            }"
+                        >
+                            {{ canton.name }}
+                        </span>
+                    </div>
+
+                    <!-- Selected canton icon -->
+                    <span
+                        v-if="canton.slug === selectedCanton.slug"
+                        class="text-primary absolute inset-y-0 right-0 flex items-center pr-4"
+                    >
+                        <!-- Heroicon name: solid/check -->
+                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                        </svg>
+                    </span>
+                </li>
+            </ul>
+        </transition>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from 'vuex';
+
+export default {
+    
+    name: 'CantonSelect',
+
+    data() {
+        return {
+            open: false,
+        };
+    },
+
+    computed: {
+        ...mapGetters([
+            'cantons',
+            'selectedCanton',
+        ]),
+    },
+
+    mounted() {
+        // Register a click event to close the menu when clicking outside of it
+        document.addEventListener('click', this.close);
+    },
+
+    beforeDestroy() {
+        // Destroy the click event listener
+        document.removeEventListener('click', this.close);
+    },
+
+    methods: {
+        ...mapMutations([
+            'setSelectedCanton',
+        ]),
+
+        // Select the given canton
+        select(canton) {
+            this.setSelectedCanton(canton);
+
+            // Close the menu
+            this.open = false;
+        },
+
+        // Close the menu
+        close(e) {
+            if (!this.$el.contains(e.target)) {
+                this.open = false;
+            }
+        },
+    },
+
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <SearchInput />
+        <div class="py-12 bg-primary">
+            <div class="flex flex-col items-center space-y-6">
+                <SearchInput />
+                <CantonSelect />
+            </div>
+        </div>
         
         <div class="mt-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
             <GentileList />
