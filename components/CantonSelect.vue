350b7225347b15a9c80537940985f40288b0aeb7
Quentin Vallat <quent.vallat@gmail.com>
2022-03-18 19:36:03 +0100

    Create solid Check and Selector icons components

--- components/CantonSelect.vue
+++ components/CantonSelect.vue
@@ -35,10 +35,7 @@
 
             <!-- Dropdown icon -->
             <span class="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                <!-- Heroicon name: solid/selector -->
-                <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
-                </svg>
+                <IconsSolidSelector class="h-5 w-5 text-primary-900 text-opacity-60" />
             </span>
         </div>
 
@@ -102,10 +99,7 @@
                         v-if="canton.slug === selectedCanton.slug"
                         class="absolute inset-y-0 right-0 flex items-center pr-4 text-primary"
                     >
-                        <!-- Heroicon name: solid/check -->
-                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-                        </svg>
+                        <IconsSolidCheck class="h-5 w-5" />
                     </span>
                 </li>
             </ul>
